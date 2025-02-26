4eac6ac2d169d1dd4ec572769842a380d1e68baa
amar-Shifan <amarshifaanvp@gmail.com>
2025-02-26 21:02:12 +0530

    types of typescript

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// types
+// String
+var name = 'Amar';
+console.log(name.toUpperCase());
+// Number 
+var num = 9.3;
+console.log(num);
+// Boolean
+var bool = true;
+console.log(bool);
---
+++ index.ts
@@ -0,0 +1,14 @@
+// types
+// String
+let name:string = 'Amar'
+console.log(name.toUpperCase());
+
+// Number 
+let num: number = 9.3
+console.log(num)
+
+// Boolean
+let bool: boolean = true
+console.log(bool)
+
+export {}
